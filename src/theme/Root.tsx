7391a1886e09b0b790000e6080776e4401856134
xuming <1769315774@qq.com>
2025-05-26 17:52:05 +0800

    fix: 修复打包失败问题

--- src/theme/Root.tsx
+++ src/theme/Root.tsx
@@ -2,17 +2,20 @@ import React from 'react';
 import { useLocation } from '@docusaurus/router';
 import { MusicProviderWrapper } from '@site/src/context/MusicContext';
 import { MusicPlayer } from 'docusaurus-player';
+import BrowserOnly from '@docusaurus/BrowserOnly';
 
 function RootContent({ children }) {
   const location = useLocation();
   const isHome = location.pathname === '/';
-  // 动态设置 initialLayout
-  const initialLayout = isHome ? 'normal' : 'floating';
 
   return (
     <>
+      {!isHome && (
+        <BrowserOnly>
+          {() => <MusicPlayer initialLayout="floating" />}
+        </BrowserOnly>
+      )}
       {children}
-      <MusicPlayer initialLayout={initialLayout} autoPlay={true} />
     </>
   );
 }
